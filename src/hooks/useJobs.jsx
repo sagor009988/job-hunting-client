7b96eb7527b77bfc8a0ac32c7251483d12e396d6
Md Saayed <mdsayed.contact@gmail.com>
2023-11-08 23:56:54 +0600

    Added blog page

--- src/hooks/useJobs.jsx
+++ src/hooks/useJobs.jsx
@@ -7,16 +7,15 @@ const useJobs = () => {
         queryKey: ['services'],
         queryFn: async () => {
             try {
-                const response1 = await axios.get('https://brand-server-pi.vercel.app/jobs');
+                const response1 = await axios.get('https://brand-server-pi.vercel.app/jobs'); 
                 const response2 = await axios.get('https://brand-server-pi.vercel.app/categories');
-
+                // send response
                 if (response1.status !== 200 || response2.status !== 200) {
                     throw new Error('Network response was not ok');
                 }
 
                 const jobs = response1.data;
                 const categories = response2.data;
-
                 return { categories, jobs };
             } catch (error) {
                 throw new Error('Network request failed: ' + error.message);
